fa0c4096b0cbecf4c21b6963d09a52b7b30cd086
sreeraj-krishnan <thesreerajrulz@gmail.com>
2018-09-15 12:56:32 +0530

    fix some issue with answer logic

--- question_answer.py
+++ question_answer.py
@@ -1,4 +1,4 @@
-
+import copy
 
 class QuestionAnswer(object):
         def __init__(self, question):
@@ -9,43 +9,46 @@ class QuestionAnswer(object):
         def find_answer(self, paragraph, possible_answers):
 
             possible_lines = set()
+            all_possible_lines = set()
+            line_count = {}
             for word in self.question.split(' '):
-                word = word.lower().strip().replace('?','').replace('\n','').replace('\r','').replace(',','')
+                word = word.lower().strip().replace('?','').replace('\n','').replace('\r','').replace(',','').replace('-','').replace(':','')
                 if word in paragraph.skip_words:
                     continue
-                #print 'question word' , word
                 if word in paragraph.keywords.keys():
-                    #print word , ' -> ' , paragraph.keywords[ word ]
-                    if len(possible_lines) == 0:
-                        possible_lines = paragraph.keywords[ word ]
-                    else:
-                        if  len(paragraph.keywords[ word ]) == 1:
+                    all_possible_lines.union(paragraph.keywords[ word ])
+                    if  len(paragraph.keywords[ word ]) == 1:
                             possible_lines = paragraph.keywords[ word ]
+                            s =  copy.deepcopy(paragraph.keywords[ word ])
+                            val = s.pop()
+                            line_count[ val ]  = 1
                             break; 
-                        else:
-                            temp = possible_lines.intersection( paragraph.keywords[ word ] )
+                    else:
+                            temp = paragraph.keywords[ word ]
+                            temp = temp.intersection( all_possible_lines )
                             if len(temp) > 0:
                                 possible_lines = temp
                             else:
                                 possible_lines = possible_lines.union( paragraph.keywords[ word ] )
+                            for l in possible_lines :
+                                if l in line_count:
+                                    line_count[ l ] += 1
+                                else:
+                                    line_count[l] = 1
                             
 
-                    #possible_lines = self.update_possible_lines( possible_lines, paragraph, word )
-                    #print possible_lines
                 else:
                     pass
-                    #print 'not present ' , word , ' in paragraph.keywords'
 
             if len(possible_lines) > 0 :
-                for answer_line in possible_lines:
-                    #print answer_line
-                    for given_answer in possible_answers.get_answers():
-                        #print paragraph.lines[answer_line].decode('utf-8') , ' -> ' , given_answer.decode('utf-8')
-                        if paragraph.lines[answer_line].decode('utf-8').lower().find(given_answer.decode('utf-8').strip().lower()) != -1:
-                            self.answer = given_answer.strip()
-                            possible_answers.answers.remove(given_answer)
-                            #print ' length now -> ' , len(possible_answers.answers)
-                            return True
+                a1_sorted_keys = sorted(line_count, key=line_count.get, reverse=True)
+                for answer_line in a1_sorted_keys:
+                    if answer_line in possible_lines:
+                        for given_answer in possible_answers.get_answers():
+                            if paragraph.lines[answer_line].decode('utf-8').lower().find(given_answer.decode('utf-8').strip().lower()) != -1:
+                                self.answer = given_answer.strip()
+                                possible_answers.answers.remove(given_answer)
+                                return True
             
             return False
     
